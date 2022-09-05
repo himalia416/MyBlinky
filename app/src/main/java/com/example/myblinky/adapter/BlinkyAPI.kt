a979eddea918bafcfc3b9c16a4b9979ce0eed04c
Aryal <himali.aryal@nordicsemi.no>
2022-09-05 14:00:00 +0200

    read characteristics

--- app/src/main/java/com/example/myblinky/adapter/BlinkyAPI.kt
+++ app/src/main/java/com/example/myblinky/adapter/BlinkyAPI.kt
@@ -1,12 +1,22 @@
 package com.example.myblinky.adapter
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCharacteristic
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.flow.Flow
+import java.util.*
 
 interface BlinkyAPI {
 
     fun disconnect()
 
-    suspend fun turnLed(on: Boolean)
+    suspend fun turnLed(device: BluetoothDevice, on: Boolean)
+//    fun onButtonStateChanged(device: BluetoothDevice, buttonState:Boolean)
 
-//    fun buttonState(): Flow<Boolean>
+    fun getButtonState(): LiveData<Boolean>
 }
--- app/src/main/java/com/example/myblinky/adapter/BluetoothLeService.kt
+++ app/src/main/java/com/example/myblinky/adapter/BluetoothLeService.kt
@@ -6,7 +6,13 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.flow.Flow
+import java.util.*
 
+val UUID_SERVICE_DEVICE = UUID.fromString("00001523-1212-efde-1523-785feabcd123")
+var buttonState = MutableLiveData<Boolean>(false)
 
 class BluetoothLeService : Service() {
     private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
@@ -15,6 +21,41 @@ class BluetoothLeService : Service() {
     private var bluetoothGatt: BluetoothGatt? = null
     private var connectionState = STATE_DISCONNECTED
     private var address: String? = null
+    private val ledState = MutableLiveData<Boolean>()
+
+    private val ledCharacteristic: BluetoothGattCharacteristic? = null
+    var ledOn: Boolean? = null
+    private val STATE_RELEASED = byteArrayOf(0x00)
+    private val STATE_PRESSED = byteArrayOf(0x01)
+
+    private val UUID_LED_CHAR = UUID.fromString("00001525-1212-efde-1523-785feabcd123")
+    private val UUID_BUTTON_CHAR = UUID.fromString("00001524-1212-efde-1523-785feabcd123")
+    private val UUID_UPDATE_NOTIFICATION_DESCRIPTOR_CHAR =
+        UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
+
+//    fun getButtonState(): LiveData<Boolean> {
+//        return buttonState
+//    }
+
+    private fun onLedStateChanged(
+        on: Boolean
+    ) {
+        ledOn = on
+        Log.d(TAG, "LED " + if (on) "ON" else "OFF")
+        ledState.value = on
+    }
+
+//    fun turnLed(on: Boolean) {
+//        if (ledCharacteristic == null) return
+//
+//        // No need to change?
+//        if (ledOn == on) return
+//        Log.d(TAG, "Turning LED " + (if (on) "ON" else "OFF") + "...")
+//        writeCharacteristic(
+//            ledCharacteristic,
+//            BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+//        )
+//    }
 
     override fun onBind(intent: Intent): IBinder {
         address = intent.getStringExtra("ADDRESS")
@@ -27,17 +68,32 @@ class BluetoothLeService : Service() {
 
         }
 
-        override suspend fun turnLed(on: Boolean) {
+        override suspend fun turnLed(device: BluetoothDevice, on: Boolean) {
 
         }
-
+        override fun getButtonState(): LiveData<Boolean> {
+            return buttonState
+        }
 //        override fun buttonState(): Flow<Boolean> {
-//
+//return
 //        }
+
 
         fun getService(): BluetoothLeService {
             return this@BluetoothLeService
         }
+    }
+    fun buttonDataCallback(status: ByteArray?){
+        if (status.contentEquals(STATE_RELEASED)){
+            buttonState.postValue(true)
+            Log.e("button data callback:", "button data callback Released ${status?.toHexString()}, buttonState ${buttonState.value}")
+        }
+        else if (status.contentEquals(STATE_PRESSED)){
+            buttonState.postValue(false)
+            Log.e("button data callback:", "button data callback Pressed ${status?.toHexString()} buttonState ${buttonState.value}")
+        }
+        else Log.e("button data callback:", "button data callback ${status?.toHexString()} and STATE_Pressed $STATE_PRESSED" +
+                "buttonState ${buttonState.value}")
     }
 
     fun initialize(): Boolean {
@@ -62,43 +118,86 @@ class BluetoothLeService : Service() {
         sendBroadcast(intent)
     }
 
+
     private val bluetoothGattCallback = object : BluetoothGattCallback() {
         override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
             val deviceAddress = gatt?.device?.address
             if (newState == BluetoothProfile.STATE_CONNECTED) {
                 // successfully connected to the GATT Server
                 connectionState = STATE_CONNECTED
-                Log.d("device connection state:", "connected to $deviceAddress")
+                Log.e("Bluetooth Gatt Callback:", "Successfully connected to $deviceAddress")
                 broadcastUpdate(ACTION_GATT_CONNECTED)
                 gatt?.discoverServices()
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                val device =
-                    Log.i("BluetoothGattCallback", "Successfully disconnected from")
+                Log.e("Bluetooth Gatt Callback", "Successfully disconnected from  $deviceAddress")
+                broadcastUpdate(ACTION_GATT_DISCONNECTED)
+                connectionState = STATE_DISCONNECTED
                 gatt?.close()// disconnected from the GATT Server
             }
         }
 
         override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
             super.onServicesDiscovered(gatt, status)
-            Log.d("device connection state:", "Services Discovered on ${gatt?.device}")
+            Log.e("Services Discovered on", "Services Discovered on ${gatt?.device}")
 
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED)
+                var gattServices: List<BluetoothGattService> =
+                    gatt?.services as List<BluetoothGattService>
+//                Log.e("onServiceDiscovered", "Services count: ${gattServices.size}")
+                for (gattService in gattServices) {
+                    var serviceUUID = gattService.uuid.toString()
+//                    Log.e("OnServicesDiscovered", "Service uuid: $serviceUUID")
+                    readCharacteristic(gattService)
+                    Log.e(
+                        "Read characteristics on:",
+                        "Read characteristics on:  $serviceUUID of $gattService"
+                    )
+                }
             } else {
-                Log.w("BluetoothService", "onServicesDiscovered received: $status")
+                Log.w(TAG, "onServicesDiscovered received: $status")
             }
-
-//            gatt?.services?.find {
-//
-//            }
         }
 
-        @Deprecated("Deprecated in Java")
         override fun onCharacteristicChanged(
             gatt: BluetoothGatt,
-            characteristic: BluetoothGattCharacteristic
+            characteristic: BluetoothGattCharacteristic,
+            value: ByteArray
         ) {
             broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic)
+            Log.d(TAG, "characteristics changed")
+            buttonDataCallback(value)
+
+        }
+
+        override fun onCharacteristicRead(
+            gatt: BluetoothGatt,
+            characteristic: BluetoothGattCharacteristic,
+            value: ByteArray,
+            status: Int
+        ) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic)
+                Log.d(TAG, "characteristics read")
+                val data = characteristic.value
+                Log.d(TAG, value.first().toString())
+//                Log.e("onCharacteristicRead", "Datos:")
+                Log.e("onCharacteristicRead", data.toHexString())
+                setCharacteristicNotification(characteristic = characteristic, true)
+
+            } else {
+                Log.i(TAG, "ACTION_DATA_READ: Error$status")
+            }
+        }
+
+        override fun onCharacteristicWrite(
+            gatt: BluetoothGatt?,
+            characteristic: BluetoothGattCharacteristic?,
+            status: Int
+        ) {
+           if (status == BluetoothGatt.GATT_SUCCESS){
+               Log.i("BluetoothGattCallback", "Wrote to characteristic ${characteristic?.uuid} ")
+           }
         }
     }
 
@@ -147,22 +246,57 @@ class BluetoothLeService : Service() {
         }
     }
 
-    fun onCharacteristicRead(
-        gatt: BluetoothGatt?,
-        characteristic: BluetoothGattCharacteristic?,
-        status: Int
-    ) {
-        if (status == BluetoothGatt.GATT_SUCCESS) {
-            characteristic?.let { broadcastUpdate(ACTION_DATA_AVAILABLE, it) }
-        }
-    }
-
-    fun readCharacteristic(characteristic: BluetoothGattCharacteristic?) {
+    fun readCharacteristic(service: BluetoothGattService?) {
         if (bluetoothGatt == null) {
-            Log.w(TAG, "BluetoothGatt not initialized")
+            Log.w(TAG, "BluetoothAdapter not initialized")
             return
         }
-        bluetoothGatt!!.readCharacteristic(characteristic)
+        val gattCharacteristic: List<BluetoothGattCharacteristic> = service!!.characteristics
+//        Log.i(TAG, "**LEO LAS ${gattCharacteristic.count()} CARACTERISTICAS DE $service**")
+        val readButtonChar =
+            bluetoothGatt!!.getService(UUID_SERVICE_DEVICE)?.getCharacteristic(UUID_BUTTON_CHAR)
+        for (characteristic in gattCharacteristic) {
+            Log.e("readCharacteristic", "Service characteristic: ${characteristic.uuid}")
+            if (characteristic.uuid == UUID_BUTTON_CHAR) {
+//                    bluetoothGatt!!.setCharacteristicNotification(characteristic, true)
+                Log.e(
+                    "Yee readCharacteristic",
+                    "Button Characteristics found: ${characteristic.uuid}"
+                )
+                bluetoothGatt!!.readCharacteristic(readButtonChar)
+                setCharacteristicNotification(characteristic = readButtonChar, true)
+            }
+        }
+        Log.e("OnServicesDiscovered", "-----------------------------")
+    }
+
+
+    fun BluetoothGattCharacteristic.isWritable(): Boolean =
+        containsProperty(BluetoothGattCharacteristic.PROPERTY_WRITE)
+
+    fun BluetoothGattCharacteristic.isWritableWithoutResponse(): Boolean =
+        containsProperty(BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE)
+
+    fun BluetoothGattCharacteristic.containsProperty(property: Int): Boolean {
+        return properties and property != 0
+    }
+
+    fun writeCharacteristic(characteristic: BluetoothGattCharacteristic?, payload: ByteArray) {
+        val writeType = when {
+            characteristic?.isWritable() == true -> BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+            characteristic?.isWritableWithoutResponse() == true -> {
+                BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE
+            }
+            else -> error("Characteristic ${characteristic?.uuid} cannot be written to")
+        }
+        if (characteristic.uuid == UUID_LED_CHAR) {
+            bluetoothGatt?.let { gatt ->
+                characteristic.writeType = writeType
+                characteristic.value = payload
+                gatt.writeCharacteristic(characteristic)
+            } ?: error("Not connected to a BLE device!")
+        }
+
     }
 
     fun setCharacteristicNotification(
@@ -174,5 +308,17 @@ class BluetoothLeService : Service() {
             return
         }
         bluetoothGatt!!.setCharacteristicNotification(characteristic, enabled)
+        if (this.UUID_BUTTON_CHAR == characteristic?.uuid) {
+            val descriptor =
+                characteristic?.getDescriptor(UUID_UPDATE_NOTIFICATION_DESCRIPTOR_CHAR)
+            descriptor?.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE)
+            bluetoothGatt?.writeDescriptor(descriptor!!)
+//            bluetoothGatt?.writeDescriptor(descriptor!!, BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE)
+        }
     }
+
+    fun ByteArray.toHexString(): String =
+        joinToString(separator = " ", prefix = "0x") { String.format("%02X", it) }
 }
+
+
