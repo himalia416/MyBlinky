662bc912e64325853e4647ea7573100b1f743be8
hiar <aryal.himali@gmail.com>
2022-09-28 16:20:49 +0200

    add all navigation in a new view model

--- app/src/main/java/com/example/myblinky/MainActivity.kt
+++ app/src/main/java/com/example/myblinky/MainActivity.kt
@@ -2,17 +2,9 @@ package com.example.myblinky
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
-import com.example.myblinky.navigation.ScanningDestinations
-import com.example.myblinky.service.rememberBoundLocalService
-import com.example.myblinky.screen.ControlDeviceScreen
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.myblinky.navigation.BleViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import no.nordicsemi.android.common.navigation.ComposeDestination
-import no.nordicsemi.android.common.navigation.ComposeDestinations
 import no.nordicsemi.android.common.navigation.DestinationId
 import no.nordicsemi.android.common.navigation.NavigationView
 import no.nordicsemi.android.common.permission.RequireBluetooth
@@ -24,41 +16,20 @@ class MainActivity : NordicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             NordicTheme {
+                val blinkyControlDestination = hiltViewModel<BleViewModel>().blinkyControlDestination
+                val scanningDestinations = hiltViewModel<BleViewModel>().scanningDestinations
                 Surface {
                     RequireBluetooth {
-                        NavigationView(destinations = ScanningDestinations + BlinkyControlDestination)
+                        NavigationView ( destinations = scanningDestinations
+                                + blinkyControlDestination
+                        )
                     }
                 }
             }
         }
     }
-
-private val blinkyControl =   ComposeDestination(ConnectView) { navigationManager ->
-    val deviceSelected = remember {
-        (navigationManager.getArgument(ConnectView) as? ConnectViewParams)?.device
-    }
-    deviceSelected?.let { device ->
-        val blinky = rememberBoundLocalService(device)
-        blinky?.let {
-            val flow = remember(blinky) {
-                blinky.getButtonState().flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
-            }
-            val buttonState by flow.collectAsState(false)
-
-            ControlDeviceScreen(
-                navigationManager = navigationManager,
-                device = device,
-                onLedChange = { blinky.turnLed(it) },
-                buttonState = buttonState,
-            )
-        }
-    }
-}
-
-    val BlinkyControlDestination = ComposeDestinations( blinkyControl )
 
     companion object {
         val Main = DestinationId(NavigationConst.HOME)
